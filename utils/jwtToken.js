fb644337f4b9d3f66a36c4baf8bbb2fe91c17e63
Sri Akhil Reddy <109200566+akhilakhil10@users.noreply.github.com>
2023-05-03 02:35:57 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,7 +9,9 @@ const sendToken = (user,statusCode,res)=>{
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
+        httpOnly: true,
+        domain: '.onrender.com' 
+
     };
     console.log(token);
    
