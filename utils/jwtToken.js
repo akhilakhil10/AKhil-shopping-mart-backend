fa03a61587d8df034883dd3effa51ca9381b227a
Sri Akhil Reddy <109200566+akhilakhil10@users.noreply.github.com>
2023-05-02 23:53:05 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,23 +1,27 @@
-//create and send token and save in the cookie
-const sendToken = (user,statusCode,res)=>{
+const sendToken = (user, statusCode, res) => {
+  // Create JWT token
+  const token = user.getJwtToken();
 
-    //create Jwt token
-    const token = user.getJwtToken();
+  // Set cookie options
+  const options = {
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: true,
+  };
 
-    //options for cookie
-    const options = {
-        expires: new Date(
-            Date.now() + 7 * 24 * 60 * 60 * 1000
-        ),
-    };
-    console.log(token);
-   
-    res.status(statusCode).cookie('token',token,options).json({
-        
-        success:true,
-        token,
-        user
-    });
-}
+  // Set the token value as a cookie in the response
+  res.cookie('token', token, options);
+
+  // Store token in local storage
+  localStorage.setItem('token', token);
+
+  // Send the response with the token value and user information
+  res.status(statusCode).json({
+    success: true,
+    token,
+    user,
+  });
+};
 
 module.exports = sendToken;
+
