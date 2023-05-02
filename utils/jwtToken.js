55c53a2fdcd5082f9f383e235ac70d1f13c3fd44
Sri Akhil Reddy <109200566+akhilakhil10@users.noreply.github.com>
2023-05-02 16:42:13 +0530

    Add files via upload

---
+++ utils/jwtToken.js
@@ -0,0 +1,23 @@
+//create and send token and save in the cookie
+const sendToken = (user,statusCode,res)=>{
+
+    //create Jwt token
+    const token = user.getJwtToken();
+
+    //options for cookie
+    const options = {
+        expires: new Date(
+            Date.now() + 7 * 24 * 60 * 60 * 1000
+        ),
+    };
+    console.log(token);
+   
+    res.status(statusCode).cookie('token',token,options).json({
+        
+        success:true,
+        token,
+        user
+    });
+}
+
+module.exports = sendToken;
