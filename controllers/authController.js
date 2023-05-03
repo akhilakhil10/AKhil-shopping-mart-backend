2ad1cfd71ece0f8672d442f1bc7e1924d169fa6f
Sri Akhil Reddy <109200566+akhilakhil10@users.noreply.github.com>
2023-05-03 22:54:00 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -135,6 +135,7 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 // Get currently logged in user details   =>   /api/v1/me
 exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
     const user = await User.findById(req.user.id);
+    console.log('Cookies:', req.cookies);
 
     res.status(200).json({
         success: true,
