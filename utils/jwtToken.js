99c6bc9b0b2df3cd9b37d3c5a0042b15b7668261
Sri Akhil Reddy <109200566+akhilakhil10@users.noreply.github.com>
2023-05-14 19:41:43 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -15,6 +15,8 @@ const sendToken = (user,statusCode,res)=>{
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         )
+           };
+    console.log(token);
  res.status(200).json({
     success: true,
     token,
