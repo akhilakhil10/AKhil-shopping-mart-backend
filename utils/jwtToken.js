df7dc3b97a4b5209b1475ad80cf09e8d924522dd
Sri Akhil Reddy <109200566+akhilakhil10@users.noreply.github.com>
2023-05-14 14:59:30 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,30 +5,28 @@ const sendToken = (user,statusCode,res)=>{
     console.log('JWT_SECRET:', process.env.JWT_SECRET);
   console.log('JWT_EXPIRES_TIME:', process.env.JWT_EXPIRES_TIME);
 
-    //create Jwt token
-    const token = user.getJwtToken();
-    
+try {
+  // create JWT token
+  const token = user.getJwtToken();
+  console.log('Token:', token);
+  // set cookie options
+  const options = {
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
+    domain: '.onrender.com'
+  };
+  // set cookie in response
+  res.cookie('token', token, options);
+  // send response with token
+  res.status(200).json({
+    success: true,
+    token,
+    user,
+  });
+} catch (err) {
+  console.error('Error generating JWT token:', err.message);
+  next(new ErrorHandler('Internal server error', 500));
+}
 
-
-
-    //options for cookie
-    const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-        ),
-          domain: 'onrender.com'
-
-    };
-    res.cookie('token', token, options);
-    console.log(token);
-   
-    res.status(statusCode).cookie('token',token,options).json({
-        
-        success:true,
-        token,
-        user
-    });
-      
     
 
 }
